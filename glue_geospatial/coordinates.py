e8a821058f9d1392ebcd0c191199afadbb8f40e1
Thomas Robitaille <thomas.robitaille@gmail.com>
2017-08-01 17:40:07 +0100

    Fix loading/saving of sessions

--- glue_geospatial/coordinates.py
+++ glue_geospatial/coordinates.py
@@ -56,19 +56,33 @@ class GeospatialLonLat2Pixel(CurvedTransform):
 
 class GeospatialLonLatCoordinates(Coordinates):
 
-    def __init__(self, raster_reader, flipud=True):
+    def __init__(self, raster_reader=None, affine=None, crs_dict=None, flipud=True):
 
-        affine = raster_reader.affine
+        if raster_reader is None:
 
-        if flipud:
-            M = raster_reader.shape[0]
-            self.affine = Affine(affine.a, -affine.b, affine.c + affine.b * M,
-                                 affine.d, -affine.e, affine.f + affine.e * M)
+            if affine is None or crs_dict is None:
+                raise ValueError("Either raster_reader or affine/crs_dict should be given")
+
         else:
-            self.affine = raster_reader.affine
 
+            if affine is not None or crs_dict is not None:
+                raise ValueError("Either raster_reader or affine/crs_dict should be given")
+
+            affine = raster_reader.affine
+
+            if flipud:
+                M = raster_reader.shape[0]
+                affine = Affine(affine.a, -affine.b, affine.c + affine.b * M,
+                                affine.d, -affine.e, affine.f + affine.e * M)
+            else:
+                affine = raster_reader.affine
+
+            crs_dict = raster_reader.crs.to_dict()
+
+        self.affine = affine
         self.iaffine = ~self.affine
-        self.proj = Proj(**raster_reader.crs.to_dict())
+        self.crs_dict = crs_dict
+        self.proj = Proj(**crs_dict)
         self.axis_labels = ['Latitude', 'Longitude']
 
     def axis_label(self, axis):
@@ -99,6 +113,13 @@ class GeospatialLonLatCoordinates(Coordinates):
 
     def dependent_axes(self, axis):
         return (0, 1)
+
+    def __gluestate__(self, context):
+        return dict(affine=list(self.affine)[:6], crs_dict=self.crs_dict)
+
+    @classmethod
+    def __setgluestate__(cls, rec, context):
+        return cls(affine=Affine(*rec['affine']), crs_dict=rec['crs_dict'])
 
 
 class MatrixCoordinates(Coordinates):
